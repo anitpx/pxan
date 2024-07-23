00fe79de15944fa370ab6463cc8556eddf11679b
anpx <164279700+anitpx@users.noreply.github.com>
2024-07-23 17:59:57 +0700

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -79,6 +79,17 @@ then
 	IPV6ADDR=2403:6a40:2:7500::$IPD:0000/56
 	IPV6_DEFAULTGW=2403:6a40:2:7500::1
 	EOF
+elif [ $IPC == 5 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:2:7100::$IPD:0000/56
+	IPV6_DEFAULTGW=2403:6a40:2:7100::1
+	EOF
 else
   echo 'KHONG THANH CONG!'
 fi
