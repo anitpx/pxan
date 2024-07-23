096a25f5d38834a6d49220296470659fa3a76ee4
anpx <164279700+anitpx@users.noreply.github.com>
2024-07-23 17:53:48 +0700

    Create ipv6.sh

---
+++ ipv6.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+YUM=$(which yum)
+#####
+if [ "$YUM" ]; then
+echo > /etc/sysctl.conf
+##
+tee -a /etc/sysctl.conf <<EOF
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.all.disable_ipv6 = 0
+EOF
+##
+sysctl -p
+IPC=$(curl -4 -s icanhazip.com | cut -d"." -f3)
+IPD=$(curl -4 -s icanhazip.com | cut -d"." -f4)
+##
+if [ $IPC == 16 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:2:1600::$IPD:0000/56
+	IPV6_DEFAULTGW=2403:6a40:2:1600::1
+	EOF
+elif [ $IPC == 17 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2400:7ea0:0:100::$IPD:0000/56
+	IPV6_DEFAULTGW=2400:7ea0:0:100::1
+	EOF
+elif [ $IPC == 141 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2400:7ea0:0:2000::$IPD:0000/56
+	IPV6_DEFAULTGW=2400:7ea0:0:2000::1
+	EOF
+elif [ $IPC == 142 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2400:7ea0:0:2400::$IPD:0000/56
+	IPV6_DEFAULTGW=2400:7ea0:0:2400::1
+	EOF
+elif [ $IPC == 13 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:2:7300::$IPD:0000/56
+	IPV6_DEFAULTGW=2403:6a40:2:7300::1
+	EOF
+elif [ $IPC == 15 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:2:7500::$IPD:0000/56
+	IPV6_DEFAULTGW=2403:6a40:2:7500::1
+	EOF
+else
+  echo 'KHONG THANH CONG!'
+fi
+
+service NetworkManager restart
+
+fi
+echo 'Da tao IPV6 thanh cong!'
