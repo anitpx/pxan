312aac25224e7329f347f4e8a7f4d691a6c51aed
anpx <164279700+anitpx@users.noreply.github.com>
2025-02-21 09:21:13 +0700

    Update ipv6ver3.sh

--- ipv6ver3.sh
+++ ipv6ver3.sh
@@ -57,6 +57,17 @@ then
 	IPV6ADDR=2400:7ea0:0:2400::$IPD:0000/56
 	IPV6_DEFAULTGW=2400:7ea0:0:2400::1
 	EOF
+elif [ $IPC == 12 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2001:0df4:2540:1000::$IPD:0000/56
+	IPV6_DEFAULTGW=2001:0df4:2540:1000::1
+	EOF
 elif [ $IPC == 13 ]
 then
    tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
